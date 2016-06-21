a7f6b35ba107b3bbfb60ea6f5d584f38497c5f3e
crashrose <crashrose@gmail.com>
2016-06-21 14:57:23 -0700

    Update Policyfile.rb

--- cookbooks/app/Policyfile.rb
+++ cookbooks/app/Policyfile.rb
@@ -1,4 +1,4 @@
-# Policyfile.rb - Describe how you want Chef to build your system.
+# PolicyFile.rb - Describe how you want Chef to build your system.
 #
 # For more information on the Policyfile feature, visit
 # https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
